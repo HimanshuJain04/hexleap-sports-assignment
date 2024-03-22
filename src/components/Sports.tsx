9558b0bf70bd97985b043a30be9031177e225f1d
Himanshu <himanshujain5690174@gmail.com>
2024-03-22 09:26:47 +0530

    init

---
+++ src/components/Sports.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+const Sports = () => {
+  return (
+    <div>
+      <div>
+        {/* heading */}
+        <div>
+          <span className="text-2xl pb-2 border-b-2 border-[#738FFF] text-white font-bold">
+            Sports
+          </span>
+        </div>
+
+        {/* cards */}
+        <div className="pt-6 flex justify-start items-start">
+          
+        </div>
+
+        {/* button */}
+        <div>
+          <button></button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Sports;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,32 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        // **************** DARK THEME ******************
+        'primary-dark': '#292B32',
+        'top-linear-dark': '#18282A',
+        'bottom-linear-dark': '#221A2C',
+        'ads-bg-dark': '#006555',
+        'bg-card-dark': '#3B3E47',
+        'bg-card-desc-dark': '#292B32',
+        'text-card-dark': '#DFDFDF',
+        'card-dots-dark': '#818A97',
+
+        // **************** LIGHT THEME ******************
+        'primary-light': '#F7F7F8',
+        'top-linear-light': '#F9F8FF',
+        'bottom-linear-light': '#F3F9FF',
+        'ads-bg-light': '#FFFFFF',
+        'bg-card-light': '#FFFFFF',
+        'bg-card-desc-light': '#F7F7F8',
+        'text-card-light': '#525965',
+        'card-dots-light': '#A9ACB2',
+
+        'color-white': '#ffffff',
+        'color-black': '#000000',
+        'bg-button': '#2C9CF0',
+
+      }
     },
   },
   plugins: [],
